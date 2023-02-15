856b58379de0646b1406a89475d98d606dc80d0c
Sam <samuel.waters@gmail.com>
2023-02-14 22:38:53 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # post "/objects", to: 'objects#create'
+  # get "/objects/:id", to: 'objects#show'
+  # get "/objects", to: 'objects#index'
+  # patch "/objects/:id", to: 'objects#update'
+  # delete "/objects/:id", to: 'objects#delete'
+
 end
