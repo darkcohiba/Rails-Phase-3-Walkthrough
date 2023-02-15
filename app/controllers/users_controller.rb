a6e6581abca6776eb3f98a021b4a94910dfdb7ff
Sam <samuel.waters@gmail.com>
2023-02-14 22:52:23 -0700

    added controller to user info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
 end
+
+# commands
+# rails g controller user
+# basic routes
+# index
+# show
+# create
+# update
+# destroy
+# private methods
+# find by id
+# params
